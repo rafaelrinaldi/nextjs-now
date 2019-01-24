22f9417f9475ce4b873888ed3eebe67e74ba80f9
Rafael Rinaldi <rafael@rinaldi.io>
2019-01-23 23:25:42 -0500

    Update based on feedback from Spectrum

--- server.js
+++ server.js
@@ -1,31 +1,7 @@
-const { createServer } = require('http')
 const { parse } = require('url')
-const next = require('next')
 
-const dev = process.env.NODE_ENV !== 'production'
-const port = parseInt(process.env.PORT, 10) || 3000
-
-const app = next({ dev })
-const handle = app.getRequestHandler()
-
-async function main (req, res) {
-  const parsedUrl = parse(req.url, true)
+module.exports = (request, response) => {
+  const parsedUrl = parse(request.url, true)
   const { pathname } = parsedUrl
-
-  if (/^\/(foo|bar)/.test(pathname)) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end(`${pathname} - test`)
-  }
-
-  handle(req, res)
+  response.end(`${pathname} - test`)
 }
-
-async function setup (handler) {
-  await app.prepare()
-  return createServer(handler).listen(port, error => {
-    if (error) throw error
-    console.log(`Server ready at "http://localhost:${port}"`)
-  })
-}
-
-setup(main)
